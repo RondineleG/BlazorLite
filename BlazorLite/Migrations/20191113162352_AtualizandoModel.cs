476800e5e42dda69d46a60fbf9a2eef3f5cbf20f
Rondinele <rondineleg@edu.univali.br>
2019-11-13 13:33:19 -0300

    Add Migratios

---
+++ BlazorLite/Migrations/20191113162352_AtualizandoModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorLite.Migrations
+{
+    public partial class AtualizandoModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
